5a97e61eb84e0173c31a96722db953c1e6148219
Ed Mathia <emathia@gmail.com>
2024-06-14 12:53:54 -0500

    Server now sends an object to website.  Website sends request and updates page from response

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require('express');
 
 const app = express();
+app.listen(3000);
 
 app.use(express.static("public"))  // This allows us to server static html files.
 app.use(express.urlencoded({ extended: true })) //This allos us to decode the request body.
 app.use(express.json()) //This allows us to parse json data.
 
 // Make a call to chatGPT to get information
-app.post('/', (req, res) => {
+app.post('/api', (req, res) => {
     console.log(req.body.inputQuery)
-    res.status(200)
+    res.send({
+        original: '<span class="verb">Original updated</span>',
+        corrected: '<span class="verb">corrected updated</span>',
+        instructions: '<span class="subject">Instructions updated</span>'
+    })
 })
 
 
 
-app.listen(3000);
 
 
 
